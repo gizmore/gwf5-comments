c4f011d938c90a4e2d4beafbd4ec55ff99f2deaa
gizmore <gizmore@wechall.net>
2017-07-15 03:20:49 +0200

    sync

--- lang/comments_en.php
+++ lang/comments_en.php
@@ -3,4 +3,8 @@ return array(
 'link_comments' => '%s comments…',
 'btn_write_comment' => 'Write&nbsp;Comment',
 'commented_at' => 'comment from %s',
+###########
+'msg_comment_added' => 'Your comment has been added.',
+####
+'ft_comments_edit' => '[%s] Edit Comment',
 );
